5bc342fc7d013d70ff617c9a18d92d4a88e58a0a
Delfina <delfinamartinezfontana@gmail.com>
2022-04-25 19:22:31 -0300

    Creando form padron 2

--- src/Controller/PadronController.php
+++ src/Controller/PadronController.php
@@ -7,6 +7,7 @@ use App\Form\PadronType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class PadronController extends AbstractController
 {
