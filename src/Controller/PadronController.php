c5d1d9381489973d9869d373049e98e5c28cc222
FachaBot <facchinielias@gmail.com>
2022-05-02 19:03:59 -0300

    Agregadas funciones de getAll, getByID y de Edit en el padron controller

--- src/Controller/PadronController.php
+++ src/Controller/PadronController.php
@@ -12,9 +12,37 @@ use Symfony\Component\HttpFoundation\Request;
 class PadronController extends AbstractController
 {
     /**
-     * @Route("/createpadron", name="app_padron")
+     * @Route("/padron", name="discapacitados")
      */
-    public function index(Request $request): Response
+    public function getAll()
+    {
+            $em = $this->getDoctrine()->getManager();
+            $discapacitados = $em->getRepository(Padron::class)->findAll();
+
+            return $this->render('padron/index.html.twig', [
+                'controller_name' => 'PadronController',
+                'discapacitados' => $discapacitados
+            ]);
+    }
+
+    /**
+     * @Route("/padron/{id}", name="padron")
+     */
+    public function getBy($id)
+    {
+            $em = $this->getDoctrine()->getManager();
+            $discapacitado = $em->getRepository(Padron::class)->find($id);
+
+            return $this->render('padron/verpadron.html.twig', [
+                'controller_name' => 'PadronController',
+                'discapacitado' => $discapacitado
+            ]);
+    }
+    
+    /**
+     * @Route("/createpadron", name="createpadron")
+     */
+    public function create(Request $request): Response
     {
         $padron = new Padron();
         $form = $this->createForm(PadronType::class, $padron);
@@ -26,9 +54,35 @@ class PadronController extends AbstractController
             $em->flush();
             $this->addFlash(type: 'exito', message: 'Se ha registrado exitosamente.');
         }
-        return $this->render('padron/index.html.twig', [
+        return $this->render('padron/create.html.twig', [
             'controller_name' => 'PadronController',
             'formulario'=>$form->createView()
         ]);
     }
+
+    /**
+     * @Route("/editpadron/{id}", name="editpadron")
+     */
+    public function edit(Request $request): Response
+    {
+        $padron = new Padron();
+        $form = $this->createForm(PadronType::class, $padron);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em =  $this->getDoctrine()->getManager();
+            $em->persist($padron);
+            $em->flush();
+            $this->addFlash('success', 'Padron Modificado');
+
+            return $this->redirectToRoute('padron');
+        }
+
+        return $this->render('padron/edit.html.twig', [
+            'controller_name' => 'PadronController',
+            'formulario' => $form->createView()
+        ]);
+    }
+
 }
