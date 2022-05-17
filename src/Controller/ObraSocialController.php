f8c7b5c7021e084032de6d0312fc415e69a1fd88
FachaBot <facchinielias@gmail.com>
2022-05-16 23:27:08 -0300

    Agregando funciones getAll, edit y delete + templates para ObraSocial

--- src/Controller/ObraSocialController.php
+++ src/Controller/ObraSocialController.php
@@ -12,9 +12,23 @@ use Symfony\Component\HttpFoundation\Request;
 class ObraSocialController extends AbstractController
 {
     /**
-     * @Route("/obrasocial", name="app_obra_social")
+     * @Route("/obrasocial", name="obrasocial")
      */
-    public function index(Request $request): Response
+    public function getAll()
+    {
+            $em = $this->getDoctrine()->getManager();
+            $obrasocial = $em->getRepository(ObraSocial::class)->findAll();
+
+            return $this->render('obra_social/index.html.twig', [
+                'controller_name' => 'ObraSocialController',
+                'obrasocial' => $obrasocial
+            ]);
+    }
+
+    /**
+     * @Route("/obrasocial/create", name="createobrasocial")
+     */
+    public function create(Request $request): Response
     {
         $obra = new ObraSocial();
         $form = $this->createForm(ObraSocialType::class, $obra);
@@ -24,10 +38,53 @@ class ObraSocialController extends AbstractController
             $em->persist($obra);
             $em->flush();
             $this->addFlash(type: 'exito', message: 'Se ha registrado exitosamente.');
+            return $this->redirectToRoute('obrasocial');
         }
-        return $this->render('obra_social/index.html.twig', [
+        return $this->render('obra_social/create.html.twig', [
             'controller_name' => 'ObraSocialController',
             'formulario'=>$form->createView()
         ]);
     }
+
+     /**
+     * @Route("/obrasocial/edit/{id}", name="editobrasocial")
+     */
+    public function edit(ObraSocial $obrasocial, Request $request, $id): Response
+    {
+        
+        $em = $this->getDoctrine()->getManager();
+        $obrasocial = $em->getRepository(ObraSocial::class)->find($id);
+        $form = $this->createForm(ObraSocialType::class, $obrasocial);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($obrasocial);
+            $em->flush();
+            $this->addFlash('success', 'Obra Social Modificada');
+
+            return $this->redirectToRoute('obrasocial', [
+                'id'=>$id
+            ]);
+        }
+
+        return $this->render('obra_social/edit.html.twig', [
+            'controller_name' => 'ObraSocialController',
+            'formulario' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/obrasocial/delete/{id}", name="deleteobrasocial")
+     */
+    public function delete($id)
+    {
+            $em = $this->getDoctrine()->getManager();
+            $obrasocial= $em->getRepository(ObraSocial::class)->find($id);
+            $em->remove($obrasocial);
+            $em->flush();
+
+            return $this->redirectToRoute('obrasocial');
+    }
+
+
 }
