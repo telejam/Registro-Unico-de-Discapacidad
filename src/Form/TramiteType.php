885d1fd1236d5ddf1e2b143cbdec2d623fabc4b0
FachaBot <facchinielias@gmail.com>
2022-05-31 22:31:14 -0300

    Agregando numeroexpediente al form de tramite

--- src/Form/TramiteType.php
+++ src/Form/TramiteType.php
@@ -10,6 +10,7 @@ use App\Entity\Usuario;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -23,6 +24,9 @@ class TramiteType extends AbstractType
         $fechaHoy->setTimezone(new \DateTimeZone('America/Argentina/Buenos_Aires'));
 
         $builder
+            ->add('numeroexpediente', IntegerType::class, [
+                'label'=>'Numero de expediente'
+            ])
             ->add('fechainicio', DateType::class, [
                 'data'  => $fechaHoy,
                 'label'=>'Fecha de inicio'
